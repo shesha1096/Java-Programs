01d94629113f7fec8ca0f8d2ff34b27903997a6d
Janakiram Shesha Shankar <jsheshashankar@manh.com>
2018-07-26 13:35:15 +0530

    Hibernate

---
+++ HibernateMvcSpringDemo/src/main/java/com/controllers/BookController.java
@@ -0,0 +1,14 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+	@RequestMapping("/")
+	public String testApp()
+	{
+		return "test";
+	}
+
+}
